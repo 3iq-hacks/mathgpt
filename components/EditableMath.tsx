5e7156df927bd78299eea0a297ae0765408ff203
Joshua Ji <joshua.ji@jiwhiz.com>
2023-01-29 07:18:48 +0000

    Fix mathquill imports

--- components/EditableMath.tsx
+++ components/EditableMath.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { EditableMathField } from 'react-mathquill'
+import { EditableMathField } from 'react18-mathquill'
 
 const EditableMathExample = () => {
     const [latex, setLatex] = useState('\\frac{1}{\\sqrt{2}}\\cdot 2')
