0992daae5b0cd09494c4119b0f038b593c8a0859
Joshua Ji <joshua.ji@jiwhiz.com>
2023-01-29 00:10:10 +0000

    delete mathlive script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,6 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <link rel="stylesheet" href="https://unpkg.com/mathlive/dist/mathlive-static.css" />
-                <script defer src="//unpkg.com/mathlive"></script>
             </Head>
             <body>
                 <ColorModeScript initialColorMode={theme.config.initialColorMode} />
