95f2fdfdfb0a2e320dd56cfee6b1298f6c998873
Joshua Ji <joshuanianji@users.noreply.github.com>
2023-02-02 15:47:13 -0700

    Update description meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -141,7 +141,7 @@ export default function Home() {
         <>
             <Head>
                 <title>Math GPT</title>
-                <meta name="description" content="Awesome math" />
+                <meta name="description" content="GPT-3 Powered Math Solutions" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
